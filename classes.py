d883efd2108e33b2de298d40adda43d119cc9d7e
Kohei Yamamoto <kh2yam1@gmial.com>
2017-10-06 17:17:14 -0400

    [add] Add classes file

---
+++ classes.py
@@ -0,0 +1,29 @@
+#import numpy as np
+
+class Individual(object):
+  
+  def __init__(self, chromosome):
+    self.chromosome = chromosome
+    self.fitness = None
+
+
+class Node(object):
+
+  def __init__(self, id_, type_, position, demand):
+    self.id_ = id_
+    self.type_ = type_
+    self.position = position
+    self.demand = demand
+
+  # Getter
+  def get_id():
+    return self.id_
+
+  def get_type():
+    return self.type_
+
+  def get_pos():
+    return self.position
+
+  def get_dem():
+    return self.demand
