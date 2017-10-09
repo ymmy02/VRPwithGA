f5b5e0c0ff9e1f1cb5680b68c27f7d36f2e37a5c
Kohei Yamamoto <kh2yam1@gmial.com>
2017-10-09 19:09:20 -0400

    [add] Add Best Cost Route Crossover

--- crossover.py
+++ crossover.py
@@ -6,6 +6,19 @@ import copy
 ###########
 # Private #
 ###########
+def _insert_node(nodes, new_chromosome, L):
+  for insert_node in L:
+    feasible_list = []
+    for (i, route) in enumerate(new_chromosome):
+      if nodes.is_feasible(route+[insert_node]):
+        feasible_list.append(i)
+    if len(feasible_list) == 0:   # Is Empty
+      new_chromosome.append([insert_node])
+    else:
+      i = random.choice(feasible_list)
+      j = random.choice(range(len(new_chromosome[i])+1))
+      new_chromosome[i].insert(j, insert_node)
+
 #===<Route Crossover>===#
 def _flatten(chromosome):
   return [node for route in chromosome for node in route]
@@ -23,21 +36,6 @@ def _mask(chromosome):
 def _rearrange_in_counterpart_order(L, ch_onerow):
   return [i for i in ch_onerow if i in L]
   
-def _insert_node(nodes, new_chromosome, L):
-  for insert_node in L:
-    feasible_list = []
-    for (i, route) in enumerate(new_chromosome):
-      if nodes.is_feasible(route+[insert_node]):
-        feasible_list.append(i)
-    if len(feasible_list) == 0:   # Is Empty
-      new_chromosome.append([insert_node])
-    else:
-      i = random.choice(feasible_list)
-      j = random.choice(range(len(new_chromosome[i])+1))
-      new_chromosome[i].insert(j, insert_node)
-
-  return new_chromosome
-
 def _route_crossover(nodes, ch1, ch2):
   L1, new_ch1 = _mask(ch1)
   L2, new_ch2 = _mask(ch2)
@@ -45,11 +43,29 @@ def _route_crossover(nodes, ch1, ch2):
   ch2_onerow = _flatten(ch2)
   L1 = _rearrange_in_counterpart_order(L1, ch2_onerow)
   L2 = _rearrange_in_counterpart_order(L2, ch1_onerow)
-  new_ch1 = _insert_node(nodes, new_ch1, L1)
-  new_ch2 = _insert_node(nodes, new_ch2, L2)
+  _insert_node(nodes, new_ch1, L1)
+  _insert_node(nodes, new_ch2, L2)
 
   return new_ch1, new_ch2
 #===</Route Crossover>===#
+
+#===<Best Cost Route Crossover>===#
+def _delete_nodes(chromosome, route):
+  for node in route:
+    for rt in chromosome:
+      if node in rt:
+        rt.remove(node)
+        break
+
+def _best_cost_route_crossover(nodes, ch1, ch2):
+  route1 = random.choice(ch1)
+  route2 = random.choice(ch2)
+  _delete_nodes(ch1, route2)
+  _delete_nodes(ch2, route1)
+  _insert_node(nodes, ch1, route2)
+  _insert_node(nodes, ch2, route1)
+  return ch1, ch2
+#===</Best Cost Route Crossover>===#
 
 ##########
 # Public #
@@ -64,6 +80,21 @@ def route_crossover(offsprings, nodes, rate=0.5):
     if random.random() < rate:
       (tmp1.chromosome, tmp2.chromosome) =        \
           _route_crossover(nodes, indv1.chromosome, indv2.chromosome)
+    new_offsprings.append(tmp1)
+    new_offsprings.append(tmp2)
+  return new_offsprings
+
+
+def best_cost_route_crossover(offsprings, nodes, rate=0.5):
+  new_offsprings = []
+  half = len(offsprings)/2
+
+  for (indv1, indv2) in zip (offsprings[0:half], offsprings[half:]):
+    tmp1 = copy.deepcopy(indv1)
+    tmp2 = copy.deepcopy(indv2)
+    if random.random() < rate:
+      (tmp1.chromosome, tmp2.chromosome) =        \
+          _best_cost_route_crossover(nodes, indv1.chromosome, indv2.chromosome)
     new_offsprings.append(tmp1)
     new_offsprings.append(tmp2)
   return new_offsprings
