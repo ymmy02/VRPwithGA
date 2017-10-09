a8472af8f037b42b158af6fb77648cc186217fd4
Kohei Yamamoto <kh2yam1@gmial.com>
2017-10-09 10:23:30 -0400

    [change] Change functions to fit the new class NodeLlist

--- functions.py
+++ functions.py
@@ -10,18 +10,18 @@ def _distance_between_nodes(node1_pos, node2_pos):
   diff = node1_pos - node2_pos
   return math.sqrt(diff.dot(diff))
 
-def _calc_one_vehicle_distance(depot, nodes, route)
+def _calc_one_vehicle_distance(nodes, route)
+  depot_pos = nodes.get_depot().get_pos()   # Assume Only one depot
   total_distance = 0
-  total_distance += _distance_between_nodes(depot, nodes[route[0]])
-
+  total_distance += \
+      _distance_between_nodes(depot_pos, nodes[route[0]].get_pos())
   for i in range(len(route)-1):
     node1_pos = nodes[route[i]].get_pos()
     node2_pos = nodes[route[i+1]].get_pos()
     total_distance += \
         _distance_between_nodes(node1_pos, node2_pos)
-
-  total_distance += _distance_between_nodes(nodes[route[-1]], depot)
-
+  total_distance += \
+      _distance_between_nodes(nodes[route[-1]].get_pos(), depot_pos)
   return total_distance
 
 
@@ -30,15 +30,9 @@ def _calc_one_vehicle_distance(depot, nodes, route)
 ##########
 def calc_distance(nodes, routes):
   total_distance = 0
-  for node in nodes:
-    if node.get_type() == 0:    # Assume Only One Depot
-      depot = node
-      break
-
   for route in routes:
     total_distance += \
-        _calc_one_vehicle_distance(depot, nodes, route)
-
+        _calc_one_vehicle_distance(nodes, route)
   return total_distance
 
 
