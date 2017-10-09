5c518e7b9d14858b3ca4779b456c6b423765bb06
Kohei Yamamoto <kh2yam1@gmial.com>
2017-10-09 19:52:52 -0400

    [add] Add data reader module

---
+++ data.py
@@ -0,0 +1,25 @@
+import xml.etree.ElementTree as ET
+import numpy as np
+
+from classes import Node, NodeList
+
+
+def loaddataset(filename):
+  filename='dataset/A-n32-k05.xml'
+  tree = ET.parse(filename)
+  root = tree.getroot()
+
+  capacity = float(root.findall('.//capacity')[0].text)   # All vehicles have the same capacity
+  nodes = NodeList(capacity)
+
+  for node, request in zip(root.iter('node'), root.iter('request')):
+    id_ = int(node.get('id'))
+    type_ = int(node.get('type'))
+    x = float(node.find('cx').text)
+    y = float(node.find('cy').text)
+    position = np.array([x, y])
+    demand = float(request.find('quantity').text)
+    node_ = Node(id_, type_, position, demand)
+    nodes.append(node_)
+
+  return nodes
